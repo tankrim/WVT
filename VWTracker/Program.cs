6deecc058728887522338c871b3896ef21177d31
tankrim <tankrim@gmail.com>
2024-07-15 08:38:19 +0200

    Add and use Serilog for logging

--- VWTracker/Program.cs
+++ VWTracker/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 namespace WVTApp
 {
     static class Program
@@ -5,26 +6,54 @@ namespace WVTApp
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Debug()
+                .WriteTo.File(
+                    path: "logs/log.json",
+                    rollingInterval: RollingInterval.Day,
+                    fileSizeLimitBytes: 10 * 1024 * 1024, // 10MB
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 7,
+                    formatter: new Serilog.Formatting.Json.JsonFormatter())
+                .CreateLogger();
 
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            Application.ThreadException += Application_ThreadException;
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            ApplicationConfiguration.Initialize();
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                Application.ThreadException += Application_ThreadException;
 
-            Application.Run(new MainForm());
+                ApplicationConfiguration.Initialize();
+
+                Application.Run(new MainForm());
+                Log.Information("WVT Started");
+            }
+            catch (Exception ex)
+            {
+
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Exception ex = (Exception)e.ExceptionObject;
+            Log.Fatal(ex, "An unhandled exception occurred");
             MessageBox.Show($"An unhandled exception occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
+            Exception ex = (Exception)e.Exception;
+            Log.Fatal(ex, "An unhandled thread exception occurred");
             MessageBox.Show($"An unhandled thread exception occurred: {e.Exception.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
